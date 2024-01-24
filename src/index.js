df78e7812649fbb3ec63971500e2c4bca3662f57
parsajaswal <parsajaswal@icloud.com>
2024-01-24 20:45:42 +0530

    UI Created

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "remixicon/fonts/remixicon.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { createRoot } from 'react-dom';
+
+const root = document.getElementById('root');
+const rootInstance = createRoot(root);
+rootInstance.render(<App />);
+
